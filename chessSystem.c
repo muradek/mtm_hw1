e10d594a19e3d9ee090555e0f6a2333b451a6657
Murad <muradek@campus.technion.ac.il>
2021-05-29 01:02:23 +0300

    implemented chess functions: Create, Destroy, AddTournament, AddGame

--- chessSystem.c
+++ chessSystem.c
@@ -10,6 +10,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+
 static bool checkLocation(const char* location)
 {
     if (strlen(location)<=0)
@@ -35,8 +36,9 @@ static bool checkLocation(const char* location)
 
 struct chess_system_t
 {
-    Map tournament_map;
-    Map game_map;
+    Map tournaments_map;
+    Map games_map;
+    Map players_map;
 };
 
 ChessSystem chessCreate()
@@ -47,36 +49,41 @@ ChessSystem chessCreate()
         return NULL;
     }
 
-    chess_system->game_map = mapCreate((copyMapDataElements) &copyGameData, (copyMapKeyElements) &copyGameKey, 
+    chess_system->games_map = mapCreate((copyMapDataElements) &copyGameData, (copyMapKeyElements) &copyGameKey, 
         (freeMapDataElements) &freeGameData, (freeMapKeyElements) &freeGameKey, (compareMapKeyElements) &compareGameKey);
     
-    chess_system->tournament_map= mapCreate((copyMapDataElements) &copyTournamentData, (copyMapKeyElements) &copyTournamentKey, 
+    chess_system->tournaments_map= mapCreate((copyMapDataElements) &copyTournamentData, (copyMapKeyElements) &copyTournamentKey, 
         (freeMapDataElements) &freeTournamentData, (freeMapKeyElements) &freeTournamentKey, (compareMapKeyElements) &compareTournamentKey);
     
+    chess_system->players_map = mapCreate((copyMapDataElements) &copyPlayerData, (copyMapKeyElements) &copyPlayerKey,
+    (freeMapDataElements) &freePlayerData, (freeMapKeyElements) &freePlayerKey, (compareMapKeyElements) &comparePlayerKey);
+
     return chess_system;
 }
 
-void chessDestroy(ChessSystem chess) //some seg fault
+void chessDestroy(ChessSystem chess)
 {
     if (!chess)
     {
         return;
     }
-    mapDestroy(chess->game_map);
-    mapDestroy(chess->tournament_map);
+
+    mapDestroy(chess->games_map);
+    mapDestroy(chess->players_map);
+    mapDestroy(chess->tournaments_map);
     free(chess);
 }
 
 ChessResult chessAddTournament(ChessSystem chess, int tournament_id, int max_games_per_player, const char* tournament_location)
 {
-    if (tournament_id<0)
+    if (tournament_id <= 0)
     {
         return CHESS_INVALID_ID;
     }
 
     TournamentKey tournament_key= tournamentKeyCreate(tournament_id);
 
-    if (mapContains(chess->tournament_map, tournament_key))
+    if (mapContains(chess->tournaments_map, tournament_key))
     {
         freeTournamentKey(tournament_key);
         return CHESS_TOURNAMENT_ALREADY_EXISTS;
@@ -88,54 +95,114 @@ ChessResult chessAddTournament(ChessSystem chess, int tournament_id, int max_gam
         return CHESS_INVALID_LOCATION;
     }
 
-    if(max_games_per_player<=0)
+    if(max_games_per_player <= 0)
     {
         freeTournamentKey(tournament_key);
         return CHESS_INVALID_MAX_GAMES;
     }
 
-    Map game_map = mapCreate((copyMapDataElements) &copyGameData, (copyMapKeyElements) &copyGameKey, 
-        (freeMapDataElements) &freeGameData, (freeMapKeyElements) &freeGameKey, (compareMapKeyElements) &compareGameKey);
+    TournamentData tournament_data= tournamentDataCreate(tournament_location, -1, max_games_per_player);
 
-    TournamentData tournament_data= tournamentDataCreate(tournament_location, -1, max_games_per_player, game_map);
-
-    mapPut(chess->tournament_map, tournament_key, tournament_data);
+    mapPut(chess->tournaments_map, tournament_key, tournament_data);
 
     return CHESS_SUCCESS;
 }
 
-/*
-int main(){
-
-ChessSystem chess= chessCreate();
-
-//chessAddTournament(chess, 7, 10, "Eilat");
-//chessAddTournament(chess, 8, 20, "Hilat");
-//chessAddTournament(chess, 6, 30, "Hilat");
-chessDestroy(chess);
-//printf("%d\n", mapGetSize(chess->tournament_map));
-return 0;
-
-}
-*/
-
-/*
-
 ChessResult chessAddGame(ChessSystem chess, int tournament_id, int first_player, int second_player, Winner winner, int play_time)
 {
-    if (tournament_id<=0 || first_player<=0 || second_player<=0)
+    if (tournament_id <= 0 || first_player <= 0 || second_player <= 0 || first_player == second_player)
     {
         return CHESS_INVALID_ID;
     }
 
-    TournamentKey tour_key= tournamentKeyCreate(tournament_id);
-    if(!mapContains(chess->tournament_map, tournament_id))
+    // check if the tournament is in the chess system
+    TournamentKey tournament_key = tournamentKeyCreate (tournament_id);
+    if (!mapContains(chess->tournaments_map, tournament_key))
     {
-        freeTournamentKey(tour_key);
+        freeTournamentKey(tournament_key);
         return CHESS_TOURNAMENT_NOT_EXIST;
     }
 
-    //tour ended??
-    //if(mapContains())
+    // check if the tournament has already ended 
+    TournamentData tournament_data = (TournamentData) mapGet(chess->tournaments_map, tournament_key);
+    if (tournament_data->ended)
+    {
+        freeTournamentData(tournament_data); // not sure if needed
+        freeTournamentKey(tournament_key);
+        return CHESS_TOURNAMENT_ENDED;
+    }
+
+    // check if the game is alredy in ths chess system
+    GameKey game_key = gameKeyCreate(first_player, second_player);
+    if (mapContains(tournament_data->games_map, game_key))
+    {
+        freeTournamentData(tournament_data); // not sure if needed
+        freeTournamentKey(tournament_key);
+        freeGameKey(game_key);
+        return CHESS_GAME_ALREADY_EXISTS;        
+    }
+
+    // check "play_time" input
+    if(play_time <= 0)
+    {
+        freeTournamentData(tournament_data); // not sure if needed
+        freeTournamentKey(tournament_key);
+        freeGameKey(game_key);
+        return CHESS_INVALID_PLAY_TIME;         
+    }
+
+    // create the data for the game we want to insert
+    GameData game_data = gameDataCreate(winner, play_time);
+
+    // insert the game to the tournament's games_map and the system's games_map
+    mapPut(chess->games_map, game_key, game_data);
+    mapPut(tournament_data->games_map, game_key, game_data);
+
+    // create players new elements for the cases of which they are not in the system/tournament
+    PlayerKey first_player_key = playerKeyCreate(first_player);
+    PlayerData first_player_data = playerDataCreate();
+    PlayerKey second_player_key = playerKeyCreate(second_player);
+    PlayerData second_player_data = playerDataCreate();
+
+
+    // if the players are not in the system, insert them to both maps
+    if (!mapContains(chess->players_map, first_player_key))
+    {
+        mapPut(chess->players_map, first_player_key, first_player_data);
+        mapPut(tournament_data->players_map, first_player_key, first_player_data);
+    }
+        
+    if (!mapContains(chess->players_map, second_player_key))
+    {
+        mapPut(chess->players_map, second_player_key, second_player_data);
+        mapPut(tournament_data, second_player_key, second_player_data);
+    }
+
+    // if the players are in the system but not in the tournament, insert only to the tournament map
+    if (mapContains(chess->players_map, first_player_key) && !mapContains(tournament_data->players_map, first_player_key))
+    {
+        mapPut(tournament_data->players_map, first_player_key, first_player_data);
+    }
+
+    if (mapContains(chess->players_map, second_player_key) && !mapContains(tournament_data->players_map, second_player_key))
+    {
+        mapPut(tournament_data->players_map, second_player_key, second_player_data);
+    }
+
+    // at this point, both players are in the system&tournament for sure
+    // update their data
+
+
+
+       
+
+    
+
+
+    return CHESS_SUCCESS;
 }
-*/
+
+int main()
+{
+    return 0;
+}
